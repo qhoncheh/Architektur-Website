9cadfa3b2261b7dc85e889bd55e913fe00fcd3b8
qhoncheh <ghonche.ataee@gmail.com>
2026-06-13 12:04:54 +0330

    .

--- src/i18n.ts
+++ src/i18n.ts
@@ -15,10 +15,10 @@ i18n.use(initReactI18next).init({
         AboutMe: "About Me",
 
         AboutMeText: `For me, architecture is much more than designing a building – it is a process of shaping spaces that enhance people's quality of life.
-In every project, I strive to create a balanced harmony between the client's requirements, the site's characteristics, and the aesthetic principles of architecture.
-With a flexible design approach and experience in various architectural styles, my goal is to develop designs that not only possess a compelling visual identity but are also functionally thoughtful and meet the real-world demands of execution.
-I view each project as an opportunity to create sustainable, characterful, and user-oriented spaces through creative and precise planning.
-`,
+        In every project, I strive to create a balanced harmony between the client's requirements, the site's characteristics, and the aesthetic principles of architecture.
+        With a flexible design approach and experience in various architectural styles, my goal is to develop designs that not only possess a compelling visual identity but are also functionally thoughtful and meet the real-world demands of execution.
+        I view each project as an opportunity to create sustainable, characterful, and user-oriented spaces through creative and precise planning.
+        `,
       },
     },
 
@@ -34,10 +34,10 @@ I view each project as an opportunity to create sustainable, characterful, and u
         AboutMe: "Über mich",
 
         AboutMeText: `Architektur ist für mich weit mehr als das Entwerfen eines Gebäudes – sie ist ein Prozess der Gestaltung von Räumen, die die Lebensqualität der Menschen verbessern.
-In jedem Projekt strebe ich danach, ein ausgewogenes Gleichgewicht zwischen den Anforderungen des Auftraggebers, den Besonderheiten des Standorts und den ästhetischen Prinzipien der Architektur zu schaffen.
-Mit einem flexiblen Gestaltungsansatz und Erfahrung in unterschiedlichen Architekturstilen ist es mein Ziel, Entwürfe zu entwickeln, die nicht nur eine überzeugende visuelle Identität besitzen, sondern auch funktional durchdacht sind und den realen Anforderungen der Ausführung gerecht werden.
-Jedes Projekt betrachte ich als eine Gelegenheit, durch kreative und präzise Planung nachhaltige, charakterstarke und nutzungsorientierte Räume zu schaffen.
-`,
+        In jedem Projekt strebe ich danach, ein ausgewogenes Gleichgewicht zwischen den Anforderungen des Auftraggebers, den Besonderheiten des Standorts und den ästhetischen Prinzipien der Architektur zu schaffen.
+        Mit einem flexiblen Gestaltungsansatz und Erfahrung in unterschiedlichen Architekturstilen ist es mein Ziel, Entwürfe zu entwickeln, die nicht nur eine überzeugende visuelle Identität besitzen, sondern auch funktional durchdacht sind und den realen Anforderungen der Ausführung gerecht werden.
+        Jedes Projekt betrachte ich als eine Gelegenheit, durch kreative und präzise Planung nachhaltige, charakterstarke und nutzungsorientierte Räume zu schaffen.
+        `,
       },
     },
   },
