e18c0dc13d820852c7ff6164b4d10691a28263ef
qhoncheh <ghonche.ataee@gmail.com>
2026-07-01 13:00:30 +0330

    .

--- src/i18n.ts
+++ src/i18n.ts
@@ -15,7 +15,6 @@ i18n.use(initReactI18next).init({
         AboutMe: "About Me",
         Previous: "Previous",
         Next:"Next", 
-
         AboutMeText: `For me, architecture is much more than designing a building – it is a process of shaping spaces that enhance people's quality of life.
         In every project, I strive to create a balanced harmony between the client's requirements, the site's characteristics, and the aesthetic principles of architecture.
         With a flexible design approach and experience in various architectural styles, my goal is to develop designs that not only possess a compelling visual identity but are also functionally thoughtful and meet the real-world demands of execution.
@@ -36,8 +35,6 @@ i18n.use(initReactI18next).init({
         AboutMe: "Über mich",
         Previous:"Zurück",
         Next:"Weiter",
-
-
         AboutMeText: `Architektur ist für mich weit mehr als das Entwerfen eines Gebäudes – sie ist ein Prozess der Gestaltung von Räumen, die die Lebensqualität der Menschen verbessern.
         In jedem Projekt strebe ich danach, ein ausgewogenes Gleichgewicht zwischen den Anforderungen des Auftraggebers, den Besonderheiten des Standorts und den ästhetischen Prinzipien der Architektur zu schaffen.
         Mit einem flexiblen Gestaltungsansatz und Erfahrung in unterschiedlichen Architekturstilen ist es mein Ziel, Entwürfe zu entwickeln, die nicht nur eine überzeugende visuelle Identität besitzen, sondern auch funktional durchdacht sind und den realen Anforderungen der Ausführung gerecht werden.
