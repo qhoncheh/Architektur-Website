a7c99fa5eae408ef73b56f969b7757ce978f4087
qhoncheh <ghonche.ataee@gmail.com>
2026-09-06 10:05:02 +0330

    .

--- src/components/about/AboutText.tsx
+++ src/components/about/AboutText.tsx
@@ -2,15 +2,12 @@ import { Typography } from "antd";
 import { useTranslation } from "react-i18next";
 
 const { Title, Paragraph } = Typography;
-
 const AboutText = () => {
   const { t } = useTranslation();
 
   return (
     <div className="flex h-full flex-col justify-center text-center">
-      <Title
-        level={3}
-        className="mb-4! text-2xl! font-light! text-white!">
+      <Title level={3} className="mb-4! text-2xl! font-light! text-white!">
         {t("AboutMe")}
       </Title>
       <Paragraph className="mb-0! whitespace-pre-line text-justify text-base! leading-7! text-[#9CA3AF]!">
